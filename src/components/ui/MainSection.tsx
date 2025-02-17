62992a7a777d6aa504c946b5763fc71dda491c5b
2taeyoon <2tae@kakao.com>
2025-02-17 16:38:37 +0900

    feat: MainSection 컴포넌트 구현 및 GSAP 기반 스크롤 애니메이션 추가

---
+++ src/components/ui/MainSection.tsx
@@ -0,0 +1,188 @@
+'use client';
+
+import Noise from "@/components/ui/Noise";
+//import SlotMachineText from "@/components/ui/SlotMachineText";
+import BigCompass from "@/components/ui/BigCompass";
+import SmallCompass from "@/components/ui/SmallCompass";
+import Wave from "@/components/ui/Wave";
+import useMouseEffect from "@/hooks/useMouseEffect";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+import Grid from "@/components/ui/Grid";
+import WordText from "@/components/ui/WordText";
+import Typewriter from "@/components/ui/Typewriter";
+
+export default function MainSection() {
+	const { gMatrix, circleMatrix, rotation, handleMouseMove } = useMouseEffect(); // 마우스 이펙트 훅
+	const scrollStartRef = useRef<HTMLDivElement | null>(null); // 스크롤 애니메이션이 적용될 요소
+
+	// const textContents = useRef(null);
+	// const firstTextRefs = useRef<(HTMLDivElement | null)[]>([]);
+	// const secondTextRefs = useRef<(HTMLDivElement | null)[]>([]);
+	
+	const gridListRefs = useRef<(HTMLDivElement | null)[]>([]); // Grid 컴포넌트의 grid_item의 ref 배열
+	const wordRefs = useRef<(SVGSVGElement | null)[]>([]); // WordText 컴포넌트의 PUBLISHER SVG Refs
+	const typewriterRefs = useRef<(HTMLDivElement | null)>(null); // Typewriter 컴포넌트의 typewriter 클래스
+	const bigCompassRef = useRef<(HTMLDivElement | null)>(null); // Typewriter 컴포넌트의 typewriter 클래스
+	const smallCompassRef = useRef<(HTMLDivElement | null)>(null); // Typewriter 컴포넌트의 typewriter 클래스
+	const smallCompassRef2 = useRef<(HTMLDivElement | null)>(null); // Typewriter 컴포넌트의 typewriter 클래스
+	const waveRef = useRef<(HTMLDivElement | null)>(null); // Wave 컴포넌트의 wave 클래스
+	const waveHeightRef = useRef<(HTMLDivElement | null)>(null); // Wave 컴포넌트의 height 클래스
+	const waveLineBlockRef = useRef<(HTMLDivElement | null)>(null); // Wave 컴포넌트의 line_block 클래스
+
+
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const ctx = gsap.context(() => {
+
+			// 초기 애니메이션 (스크롤 없이 실행)
+			gsap.fromTo( // grid_list 클래스
+				gridListRefs.current,
+				{ opacity: 0 }, // 시작 상태
+				{ opacity: 1, delay: 1, duration: 3, ease: "power2.out" } // 목표 상태
+			);
+
+			const mainScroll = gsap.timeline({
+				scrollTrigger: {
+					trigger: scrollStartRef.current,
+					start: "top top", // 트리거 요소의 A가 뷰포트(windiw)의 B에 닿을 때 시작
+					end: "bottom+=10000 top", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료 
+					//end: `"+= ${document.querySelector('.main_section')?.scrollHeight}"`,
+					scrub: 3, // 값이 클수록 더 부드럽게 움직임(기본값 true)
+					pin: true, // 스크롤 애니메이션 진행 중 스크롤 고정
+					markers: true, // 스크롤 확인용(개발 모드에서만 사용)
+				},
+			});
+
+			// Typewriter 컴포넌트의 typewriter 클래스 참조
+			mainScroll.to(typewriterRefs.current, {
+				opacity: 0,
+				duration: 2,
+			});
+
+			// Grid 컴포넌트의의 grid_list 클래스 값 변경
+			mainScroll.to(gridListRefs.current, {
+				y: +1000,
+				duration: 2, // 애니메이션 작동 시간간
+				stagger: 0.2, // 요소들이 차례대로 작동
+				ease: "power1.inOut",
+			}, "<"); // "<" 이전 애니메이션과 동시에 실행
+
+			// WordText 컴포넌트의 wordRefs strokeDasharray 값 변경
+			mainScroll.to(wordRefs.current, {
+				y: +1000,
+				duration: 2, // 3초 애니메이션 실행
+				stagger: 0.2,
+				ease: "power1.inOut",
+			}, "<");
+
+
+			// big_compass rotate 변경
+			mainScroll.to(bigCompassRef.current, {
+				rotate: 180,
+				duration: 2,
+			})
+			// small_compass 클래스 rotate 변경
+			mainScroll.to(smallCompassRef.current, {
+				rotate: 180,
+				duration: 2,
+			}, "<");
+			// small_compass2 클래스 rotate 변경
+			mainScroll.to(smallCompassRef2.current, {
+				rotate: 180,
+				duration: 2,
+			}, "<");
+
+
+			// Wave 컴포넌트의 height 클래스 참조
+			mainScroll.to(waveRef.current, {
+				height: '36rem',
+			});
+
+
+			// big_compass rotate 변경
+			mainScroll.to(bigCompassRef.current, {
+				rotate: 360,
+				duration: 2,
+			})
+			// small_compass 클래스 rotate 변경
+			mainScroll.to(smallCompassRef.current, {
+				rotate: 360,
+				duration: 2,
+			}, "<");
+			// small_compass2 클래스 rotate 변경
+			mainScroll.to(smallCompassRef2.current, {
+				rotate: 360,
+				duration: 2,
+			}, "<");
+
+
+			// Wave 컴포넌트의 height 클래스, line_black 클래스 참조
+			mainScroll.to(waveHeightRef.current, {
+				height: '20rem',
+			});
+			mainScroll.to(waveLineBlockRef.current, {
+				height: '21rem',
+			}, "<");
+			mainScroll.to(waveHeightRef.current, {
+				height: '40rem',
+			});
+			mainScroll.to(waveLineBlockRef.current, {
+				height: '41rem',
+			}, "<");
+			mainScroll.to(waveHeightRef.current, {
+				height: '80rem',
+			});
+			mainScroll.to(waveLineBlockRef.current, {
+				height: '81rem',
+			}, "<");
+
+		});
+
+		return () => ctx.revert();
+	}, []);
+
+	return (
+		<div className="main_section" onMouseMove={handleMouseMove} ref={scrollStartRef}>
+			{/* <div className="slot_machine_text" ref={textContents}>
+				<SlotMachineText firstTextRefs={firstTextRefs} secondTextRefs={secondTextRefs}/>
+			</div> */}
+			<div className="big_compass" ref={bigCompassRef}>
+				<BigCompass gMatrix={gMatrix} circleMatrix={circleMatrix}/>
+			</div>
+			<div className="small_compass" ref={smallCompassRef}>
+				<SmallCompass rotation={rotation}/>
+				<SmallCompass rotation={rotation}/>
+			</div>
+			<div className="small_compass small_compass2" ref={smallCompassRef2}>
+				<SmallCompass rotation={rotation}/>
+				<SmallCompass rotation={rotation}/>
+			</div>
+			<div className="wave_wrap">
+				<Wave waveRef={waveRef} waveHeightRef={waveHeightRef} waveLineBlockRef={waveLineBlockRef}/>
+			</div>
+			<div className="grid_wrap">
+				<Grid
+					gridColumns={[
+						{ gap: '10rem', paddingTop: '15rem', listConfig: [4, 2], scrollStartRef, gridListRefs }, // 첫번째 grid_list에 grid_item 4개 두 번째 grid_list에 grid_item 2개
+						{ gap: '15rem', paddingTop: '10rem', listConfig: [3, 3], scrollStartRef, gridListRefs }, // 첫번째 grid_list에 grid_item 3개 두 번째 grid_list에 grid_item 3개
+						{ gap: '20rem', paddingTop: '5rem', listConfig: [4, 3], scrollStartRef, gridListRefs }, // 첫번째 grid_list에 grid_item 4개 두 번째 grid_list에 grid_item 3개
+						{ gap: '10rem', paddingTop: '10rem', listConfig: [3, 3], scrollStartRef, gridListRefs }, // 첫번째 grid_list에 grid_item 3개 두 번째 grid_list에 grid_item 3개
+						{ gap: '20rem', paddingTop: '15rem', listConfig: [3, 2], scrollStartRef, gridListRefs }, // 첫번째 grid_list에 grid_item 3개 두 번째 grid_list에 grid_item 2개
+						{ gap: '25rem', paddingTop: '5rem', listConfig: [3, 3], scrollStartRef, gridListRefs } // 첫번째 grid_list에 grid_item 3개 두 번째 grid_list에 grid_item 3개
+					]}
+				/>
+			</div>
+			<div className="word_wrap">
+				<WordText wordRefs={wordRefs}/>
+			</div>
+			<div className="typewriter_wrap">
+				<Typewriter typewriterRefs={typewriterRefs}/>
+			</div>
+		<Noise/>
+	</div>
+	)
+}
