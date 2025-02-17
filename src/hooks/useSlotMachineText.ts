08eea2393436ffa262bcb9a5d30b84b2c46ba514
2taeyoon <2tae@kakao.com>
2025-02-17 16:23:32 +0900

    feat: 랜덤 문자 변환 및 최종 텍스트 애니메이션 기능 구현

--- src/hooks/useSlotMachineText.ts
+++ src/hooks/useSlotMachineText.ts
@@ -1,51 +1,58 @@
 'use client';
 
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import gsap from "gsap";
 import { SlotMachineTextProps } from "@/types/props";
 
 //https://gsap.com/docs/v3/Eases
-export default function useSlotMachineText({firstText, secondText, IterationCount, RunningInterval}: SlotMachineTextProps) {
-	const textRefs = useRef<(HTMLDivElement | null)[]>([]); // 개별 문자 요소들의 ref 배열
+export default function useSlotMachineText({firstText, secondText, IterationCount, RunningInterval, delay}: SlotMachineTextProps) {
+	const slotText = useRef<(HTMLDivElement | null)[]>([]); // 개별 문자 요소들의 ref 배열
 	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // 랜덤 문자 집합
+	const [isVisible, setIsVisible] = useState(false); // 초기에는 숨김 상태
 
 	useEffect(() => {
 		let iterations = 0; // 반복 횟수 추적하기 위한 변수
 
-		const interval = setInterval(() => {
-			textRefs.current.forEach((ref) => {
-				if (ref) {
-					const randomChar = chars[Math.floor(Math.random() * chars.length)]; // 랜덤 문자 선택
-					ref.innerText = randomChar;
+		const delayTimeout = setTimeout(()=> {
+			setIsVisible(true); // delay 이후 요소 보임
 
-					// 텍스트 크기 점점 커지는 애니메이션
-					gsap.to(ref, { color: '#03c75a', fontSize: 72 + iterations * 0.5, filter: `blur(${10 - iterations * 0.2}px)`, opacity: 1, duration: 0.3 });
-				}
-			});
-
-			iterations++;
-
-			if (iterations > IterationCount) { // 반복 횟수
-				clearInterval(interval);
-
-				textRefs.current.forEach((ref, index) => {
+			const interval = setInterval(() => {
+				slotText.current.forEach((ref) => {
 					if (ref) {
-						const allText = firstText + secondText; // 모든 텍스트 합치기
-						ref.innerText = allText[index]; // ✅ 최종 목표 문자로 변경
-						gsap.to(ref, {
-							opacity: 1,
-							duration: 0.5,
-							onComplete: () => {
-								applyFinalAnimation(ref, index)
-							}
-						}); // 애니메이션 적용
+						const randomChar = chars[Math.floor(Math.random() * chars.length)]; // 랜덤 문자 선택
+						ref.innerText = randomChar;
+	
+						// 텍스트 크기 점점 커지는 애니메이션
+						gsap.to(ref, { color: "var(--thema)", fontSize: `${(7.2 + iterations * 0.05)}rem`, filter: `blur(${10 - iterations * 0.2}px)`, duration: 0.3 });
 					}
 				});
-			}
-		}, RunningInterval); // 함수의 실행 간격
+	
+				iterations++;
+	
+				if (iterations > IterationCount) { // 반복 횟수
+					clearInterval(interval);
+	
+					slotText.current.forEach((ref, index) => {
+						if (ref) {
+							const allText = firstText + secondText; // 모든 텍스트 합치기
+							ref.innerText = allText[index]; // ✅ 최종 목표 문자로 변경
+							gsap.to(ref, {
+								opacity: 1,
+								duration: 0.5,
+								onComplete: () => {
+									applyFinalAnimation(ref, index)
+								}
+							}); // 애니메이션 적용
+						}
+					});
+				}
+			}, RunningInterval); // 함수의 실행 간격
 
-		return () => clearInterval(interval); // 언마운트 시 해제
-	}, [firstText, secondText, IterationCount, RunningInterval]); // firstText, secondText 변경 시 애니메이션 재실행행
+			return () => clearInterval(interval); // 언마운트 시 해제
+		}, delay); // 딜레이 시간
+
+		return () => clearTimeout(delayTimeout); // 언마운트 시 해제
+	}, [firstText, secondText, IterationCount, RunningInterval, delay]); // firstText, secondText 변경 시 애니메이션 재실행
 
 	const applyFinalAnimation = (ele: HTMLDivElement, index: number) => {
 		gsap.to(ele, {
@@ -58,5 +65,5 @@ export default function useSlotMachineText({firstText, secondText, IterationCoun
 		});
 	};
 
-	return { textRefs };
+	return { slotText, isVisible };
 }
