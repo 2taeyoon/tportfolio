cc6aed8ef85ef7236f2d4edfa741bb666126e228
2taeyoon <2tae@kakao.com>
2025-03-19 07:01:29 +0900

    feat: 경험란 리스트 컴포넌트 추가

---
+++ src/components/list/ExperienceItem.tsx
@@ -0,0 +1,22 @@
+import { ExperienceItemProps } from "@/types/props";
+
+export default function ExperienceItem({ title, items, extra, translate }: ExperienceItemProps) {
+  return (
+    <div className="experience_item" style={{ transform: `translate(${translate.x}, ${translate.y})` }}>
+      <div className="experience_title">{title}{extra && <span>{extra}</span>}</div>
+			<div className="content_wrap">
+				{items.map((item, index) => (
+					<div className="content" key={index}>
+						<div className="content_sub_title">{item.subTitle}</div>
+						<div className="content_detail">{item.body}</div>
+						<div className="content_meta">
+							{ item.meta.first && <div className="content_meta_item">{item.meta.first}</div> }
+							{ item.meta.second && <div className="content_meta_item">{item.meta.second}</div> }
+							{ item.meta.third && <div className="content_meta_item">{item.meta.third}</div> }
+						</div>
+					</div>
+				))}
+			</div>
+    </div>
+  );
+}
