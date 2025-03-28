9b0384bcd2f2cf8c23fd74b44a38c3055b2b8e19
2taeyoon <2tae@kakao.com>
2025-03-28 16:24:41 +0900

    chore: markers 주석 처리

--- src/components/layout/SkillSection.tsx
+++ src/components/layout/SkillSection.tsx
@@ -65,7 +65,7 @@ export default function SkillSection() {
 					start: "top bottom", // 트리거 요소의 A가 뷰포트(windiw)의 B에 닿을 때 시작
 					end: "top top+=400", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
 					scrub: 3, // 값이 클수록 더 부드럽게 움직임(기본값 true)(스크롤 반대 시에도 애니메이션 작동)
-					markers: true, // 스크롤 확인용(개발 모드에서만 사용)
+					//markers: true, // 스크롤 확인용(개발 모드에서만 사용)
 				},
 			});
 
@@ -77,7 +77,7 @@ export default function SkillSection() {
 					start: "top bottom", // 트리거 요소의 A가 뷰포트(windiw)의 B에 닿을 때 시작
 					end: "top top+=400", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
 					scrub: 3, // 값이 클수록 더 부드럽게 움직임(기본값 true)(스크롤 반대 시에도 애니메이션 작동)
-					markers: true, // 스크롤 확인용(개발 모드에서만 사용)
+					//markers: true, // 스크롤 확인용(개발 모드에서만 사용)
 				},
 			});
 
