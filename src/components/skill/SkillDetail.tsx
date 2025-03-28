c50e7682b97fd83da5efdc1d429f61e0e64b0efa
2taeyoon <2tae@kakao.com>
2025-03-28 16:04:28 +0900

    feat: SkillDetail 컴포넌트 구현 및 activeIndex 기반 상세 정보 출력

---
+++ src/components/skill/SkillDetail.tsx
@@ -0,0 +1,17 @@
+import { skillItemsData } from "@/data/skillItemdata";
+import { SkillDetailProps } from "@/types/props";
+import SkillDetailImageItem from "./list/SkillDetailImageItem";
+
+export default function SkillDetail({radius, itemSize, activeIndex}: SkillDetailProps) {
+	return (
+		<div className="skill_detail_wrap" style={{ width: `${radius * 2 -100}px`, height: `${radius * 2 -100}px`,}}>
+			<div className="skill_detail">
+				<SkillDetailImageItem itemSize={itemSize} skillItemsData={skillItemsData} activeIndex={activeIndex}/>
+				<div className="skill_detail_text">
+					<div className="skill_detail_title">{skillItemsData[activeIndex].title}</div>
+					<div className="skill_detail_body">{skillItemsData[activeIndex].body}</div>
+				</div>
+			</div>
+		</div>
+	)
+}
