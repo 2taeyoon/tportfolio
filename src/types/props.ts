ad88cd90ea5ac701564901af1122c1f62fab5b46
2taeyoon <2tae@kakao.com>
2025-03-28 15:59:07 +0900

    feat: 스킬 섹션 props 추가

--- src/types/props.ts
+++ src/types/props.ts
@@ -72,3 +72,43 @@ export interface ExperienceItemProps {
 		y: string;
 	}
 }
+
+export interface SkillCircleProps {
+	radius: number;
+	itemSize: number;
+	activeIndex: number;
+	setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export interface SkillCircleImageItemProps {
+	items: {
+    src: string;
+    shadowColors: string;
+    title: string;
+    body: string;
+  };
+	index: number;
+	angleStep: number;
+	isDraggingRef: boolean;
+	radius: number;
+	itemSize: number;
+	rotation: number;
+	activeIndex: number;
+}
+
+export interface SkillDetailProps {
+	radius: number;
+	itemSize: number;
+	activeIndex: number;
+};
+
+export interface SkillDetailImageProps {
+	skillItemsData: {
+		src: string;
+		shadowColors: string;
+		title: string;
+		body: string;
+	}[];
+	itemSize: number;
+	activeIndex: number;
+}
