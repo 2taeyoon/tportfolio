7655f3555cad36307cccc74bda73e7df6c407eb2
2taeyoon <2tae@kakao.com>
2025-03-28 16:04:06 +0900

    feat: SkillCircle 컴포넌트 구현 및 드래그 회전에 따른 이미지 배치 로직 적용

---
+++ src/components/skill/SkillCircle.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { skillItemsData } from "@/data/skillItemdata";
+import { SkillCircleProps } from "@/types/props";
+import { useSkillCircle } from "@/hooks/useSkillCircle";
+import SkillCircleImageItem from "@/components/skill/list/SkillCircleImageItem";
+
+export default function SkillCircle({ radius, itemSize, activeIndex, setActiveIndex }: SkillCircleProps) {
+	const { angleStep, skillCircleRef, isDraggingRef, rotation, normalize, handleMouseDown, handleMouseMove, handleMouseUp } = useSkillCircle({setActiveIndex});
+
+	useEffect(() => {
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseup', handleMouseUp);
+		};
+	}, [handleMouseMove, handleMouseUp]);
+
+	return (
+		<div className="skill_circle" ref={skillCircleRef} onMouseDown={handleMouseDown}
+			style={{
+				width: `${radius * 2}px`, height: `${radius * 2}px`,
+				transform: `rotate(${normalize(rotation)}deg)`
+			}}
+		>
+			{skillItemsData.map((items, index) => (
+				<SkillCircleImageItem key={index} items={items} index={index} angleStep={angleStep} isDraggingRef={isDraggingRef.current} radius={radius} itemSize={itemSize} rotation={rotation} activeIndex={activeIndex}/>
+			))}
+		</div>
+	)
+}
