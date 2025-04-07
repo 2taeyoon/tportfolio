8a12e6111217c6f7a740ee276c1e84a240cafe16
2taeyoon <2tae@kakao.com>
2025-04-08 07:00:19 +0900

    feat: ScreenshotGallery 컴포넌트 추가 - 스크린샷 갤러리를 슬라이더로 표시하고 이미지 클릭 시 모달로 확대 보기

---
+++ src/components/project/ScreenshotGallery.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import ImageModal from "@/components/modal/ImageModal";
+import { ScreenshotGalleryProps } from "@/types/projectProps";
+
+export default function ScreenshotGallery({ project, title, images, modalProps, onImageClick }: ScreenshotGalleryProps) {
+  return (
+    <div className="screenshot_container">
+      <div className="screenshot_title" style={{ color: project?.fontColor }}>{title}</div>
+      <Swiper
+        loop={false}
+        slidesPerView={3.5}
+        spaceBetween={10}
+        freeMode={true}
+      >
+        {images.map((screenshot, index) => (
+          <SwiperSlide key={index}>
+            <div className="screenshot_image" onClick={() => onImageClick(index)}>
+              <Image 
+                src={screenshot.src} 
+                alt={screenshot.alt} 
+                draggable={false} 
+                fill
+                sizes="100%"
+                style={{ objectFit: 'cover' }}
+                onDragStart={(e) => e.preventDefault()}
+                onContextMenu={(e) => e.preventDefault()}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <ImageModal
+        isOpen={modalProps.isModalOpen}
+        onClose={modalProps.handleCloseModal}
+        screenshots={images.map(img => img.src)}
+        currentImageIndex={modalProps.currentImageIndex}
+        onPrev={modalProps.handlePrevImage}
+        onNext={modalProps.handleNextImage}
+      />
+    </div>
+  );
+}
