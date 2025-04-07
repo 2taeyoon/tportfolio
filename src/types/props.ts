98ac71912e1d86da8537eae0956f9beb4e9fe517
2taeyoon <2tae@kakao.com>
2025-04-07 21:16:58 +0900

    fix: 빌드 오류 해결

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,12 +1,13 @@
-export type SquareGridProps = {
-	count?: number; // 기본값: 12줄
-  direction: "horizontal" | "vertical";
-};
-
 export interface SlotMachineTextProps {
 	firstText: string;
 	secondText: string;
 	IterationCount: number;
 	RunningInterval: number;
 	delay: number;
+}
+
+export interface GsabRefProps {
+	firstTextRefs?: RefObject<(HTMLDivElement | null)[]>;
+	secondTextRefs?: RefObject<(HTMLDivElement | null)[]>;
+	textRef?: RefObject<(HTMLDivElement | null)[]>;
 }
