7f0fa835b528fd624dbd4d7f9a1203fe5ec5dcfc
2taeyoon <2tae@kakao.com>
2025-04-08 07:08:34 +0900

    fix: 빌드 오류 해결

--- src/components/project/TechStack.tsx
+++ src/components/project/TechStack.tsx
@@ -6,9 +6,9 @@ export default function TechStack({ project, rows }: TechStackProps) {
     <div className="tech_stack">
       {rows.map((row, rowIndex) => (
         <div key={rowIndex} className="tech_row">
-          <div className="tech_row_title" style={{ color: project.fontColor }}>{row.title}</div>
+          <div className="tech_row_title" style={{ color: project?.fontColor }}>{row.title}</div>
           {row.items.map((item, itemIndex) => (
-            <div key={itemIndex} className="tech_row_text" style={{ color: project.fontColor }}>{item}</div>
+            <div key={itemIndex} className="tech_row_text" style={{ color: project?.fontColor }}>{item}</div>
           ))}
         </div>
       ))}
