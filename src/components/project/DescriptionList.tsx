900acd53574f97535330aaefae33b366b333da61
2taeyoon <2tae@kakao.com>
2025-04-14 03:40:34 +0900

    refactor: ContributionList와 FeatureList를 DescriptionList로 통합하여 재사용성 향상

---
+++ src/components/project/DescriptionList.tsx
@@ -0,0 +1,24 @@
+import { ProjectDetailCardProps } from "@/types/projectProps";
+
+export default function DescriptionList({ project, title, body, listType }: ProjectDetailCardProps) {
+  const containerClass = `${listType}_container`;
+  const titleClass = `${listType}_title`;
+  const listClass = listType;
+  const itemClass = `${listType}_item`;
+  const ListContainer = listType === 'features' ? 'div' : 'ol';
+
+  return (
+    <div className={containerClass}>
+      <div className={titleClass} style={{ color: project?.fontColor }}>{title}</div>
+      <ListContainer className={listClass}>
+        {body?.map((item, index) => (
+          listType === 'features' ? (
+            <div key={index} className={itemClass} style={{ color: project?.fontColor }}>{item}</div>
+          ) : (
+						<li key={index} className={itemClass} style={{ color: project?.fontColor }}>{item}</li>
+          )
+        ))}
+      </ListContainer>
+    </div>
+  );
+} 
