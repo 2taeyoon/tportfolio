fe9e4c693a9f29796d565907234841481dfd4905
2taeyoon <2tae@kakao.com>
2025-04-14 03:43:19 +0900

    refactor: FeatureList, ContributionList를 DescriptionList로 통합하고 컴포넌트명을 ProjectDetailCard로 변경

--- src/components/project/ProjectDetailCard.tsx
+++ src/components/project/ProjectDetailCard.tsx
@@ -1,16 +1,27 @@
 import { useImageModal } from "@/hooks/useImageModal";
 import TechStack from "@/components/project/TechStack";
 import SkillList from "@/components/project/SkillList";
-import FeatureList from "@/components/project/FeatureList";
 import ScreenshotGallery from "@/components/project/ScreenshotGallery";
 import ProjectLinks from "@/components/project/ProjectLinks";
-import { ProjectCardItemProps } from "@/types/projectProps";
-import ContributionList from "./ContributionList";
+import { ProjectDetailCardProps } from "@/types/projectProps";
+import DescriptionList from "@/components/project/DescriptionList";
 
-export default function ProjectCardItem({ project, index }: ProjectCardItemProps) {
+export default function ProjectDetailCard({ project, index }: ProjectDetailCardProps) {
 	const { isModalOpen, currentImageIndex, handleImageClick, handlePrevImage, handleNextImage, handleCloseModal } = useImageModal();
 
 	const getCardClass = (baseClass: string) => {
+		if (project?.className === 'typortfolio'){
+      return `${baseClass} typortfolio`;
+    }
+		if (project?.className === '2taeyoon'){
+      return `${baseClass} 2taeyoon`;
+    }
+		if (project?.className === 'krmx'){
+      return `${baseClass} krmx`;
+    }
+		if (project?.className === 'hivu'){
+      return `${baseClass} hivu`;
+    }
 		if (project?.className === 'niweb'){
       return `${baseClass} niweb`;
     }
@@ -35,9 +46,9 @@ export default function ProjectCardItem({ project, index }: ProjectCardItemProps
 			<div className="project_card_type" style={{ backgroundColor: project.badgeBoxColor, color: project.badgeFontColor }}>{project.type}</div>
 			<TechStack project={project} rows={project.techStack.rows}/>
 			<SkillList project={project} title={project.skills.title} images={project.skills.images}/>
-			<FeatureList project={project} title={project.features.title || ''} body={project.features.body || []}/>
+			<DescriptionList project={project} title={project.features.title || ''} body={project.features.body || []} listType="features"/>
 			{project.contribution && (
-				<ContributionList project={project} title={project.contribution.title || ''} body={project.contribution.body || []}/>
+				<DescriptionList project={project} title={project.contribution.title || ''} body={project.contribution.body || []} listType="contributions"/>
 			)}
 			<ScreenshotGallery project={project} title={project.screenshots.title} images={project.screenshots.images}
 				modalProps={{ isModalOpen, currentImageIndex, handleCloseModal, handlePrevImage, handleNextImage }}
