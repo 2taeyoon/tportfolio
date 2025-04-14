cd0969ced34688956486df8ab69e7f85c887835b
2taeyoon <2tae@kakao.com>
2025-04-14 12:43:47 +0900

    refactor: 프로젝트 링크 버튼 스타일 로직을 getClassName 유틸 함수로 분리하여 가독성 개선

--- src/components/project/ProjectLinks.tsx
+++ src/components/project/ProjectLinks.tsx
@@ -1,35 +1,18 @@
 import { ProjectLinksProps } from "@/types/projectProps";
+import getClassName from "@/utils/getClassName";
 import Link from 'next/link';
 
 export default function ProjectLinks({ project, site, github }: ProjectLinksProps) {
-  if (!site && !github) return null;
-
-  const getLinkClass = (baseClass: string) => {
-		const widthClass = site && github ? 'width49' : 'width100'
-		let themaClass = '';
-
-    if (project?.linkBoxColor === 'var(--whiteDE)') {
-      themaClass = 'whiteDE';
-    } else if (project?.linkBoxColor === 'var(--themaDeep)') {
-      themaClass = 'themaDeep';
-    } else if (project?.linkBoxColor === 'var(--thema)') {
-      themaClass = 'thema';
-    } else if (project?.linkBoxColor === 'var(--black333)') {
-      themaClass = 'black333';
-    }
-    return `${baseClass} ${themaClass} ${widthClass}`;
-  };
-
   return (
     <div className="link_shortcut">
       {site && (
-        <Link href={site.href} className={getLinkClass('site')} target="_blank" rel="noopener noreferrer"
+        <Link href={site.href} className={getClassName('site', project)} target="_blank" rel="noopener noreferrer"
 					style={{ backgroundColor: project?.linkBoxColor, color: project?.linkFontColor, }}>
           {site.text}
         </Link>
       )}
       {github && (
-        <Link href={github.href} className={getLinkClass('github')} target="_blank" rel="noopener noreferrer"
+        <Link href={github.href} className={getClassName('github', project)} target="_blank" rel="noopener noreferrer"
 					style={{ backgroundColor: project?.linkBoxColor, color: project?.linkFontColor, }}>
           {github.text}
         </Link>
