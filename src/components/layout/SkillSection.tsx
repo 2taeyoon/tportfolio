c0c3ff1e4f161639356d7e7dd1772e27b3fea08e
2taeyoon <2tae@kakao.com>
2025-04-14 12:48:17 +0900

    refactor: TextSplitWrap 구현으로 스플릿 텍스트 전부 TextSplitWrap로 변경

--- src/components/layout/SkillSection.tsx
+++ src/components/layout/SkillSection.tsx
@@ -9,10 +9,14 @@ import SkillDetail from "@/components/skill/SkillDetail";
 import DividerWithIcons from "@/components/common/DividerWithIcons";
 import RadialGradient from "@/components/common/RadialGradient";
 import useMouseEffect from "@/hooks/useMouseEffect";
-import MySkillBody from "@/components/skill/MySkillText";
 import Wave from "../common/Wave";
+import { spaceTextSplit } from "@/utils/textSplit";
+import TextSplitWrap from "../common/TextSplitWrap";
 
 export default function SkillSection() {
+	const title = `MY SKILL`;
+  const body = `시맨틱 태그를 활용한 구조적인 마크업 작성에 능숙하며 CSS(SCSS, Tailwind CSS)를 통해 반응형 웹, 애니메이션, 일관된 디자인 시스템을 구현할 수 있습니다. JavaScript와 TypeScript를 활용한 동적 UI 및 사용자 인터랙션 구현이 가능하며 React와 Next.js 기반의 컴포넌트 중심 개발과 SSR/SSG 최적화 경험을 갖추고 있습니다. Webpack을 통한 빌드 환경을 구성할 수 있으며 Git과 GitHub을 활용한 협업에도 익숙합니다. Firebase 및 Supabase를 통한 백엔드 연동도 경험해 보았습니다. Photoshop, Illustrator로 시각적 콘텐츠를 제작할 수 있으며 Markdown으로 Notion 문서 및 콘텐츠 작성도 가능합니다.`;
+
 	const skillScrollRef = useRef<HTMLDivElement | null>(null); // 스크롤 애니메이션이 적용될 요소
 
 	const [activeIndex, setActiveIndex] = useState(0); // 현재 가장 위에 있는 이미지 인덱스 (0번부터 시작)
@@ -103,7 +107,8 @@ export default function SkillSection() {
     <div className="skill_section section inner" ref={skillScrollRef} onMouseMove={handleMouseMove}>
 			<DividerWithIcons/>
 			<div className="my_skill_wrap pd_box">
-				<MySkillBody/>
+				<TextSplitWrap text={title} className="my_skill_title" splitFunction={spaceTextSplit}/>
+				<TextSplitWrap text={body} className="my_skill_body" splitFunction={spaceTextSplit}/>
 			</div>
 			<RadialGradient translate2={translate2} className="left"/>
 			<RadialGradient translate2={translate2} className="right"/>
