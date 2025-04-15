aebcadb868b1d3bd923923f15e8653a0ef624e64
2taeyoon <2tae@kakao.com>
2025-04-15 18:01:30 +0900

    feat: TextSplitWrap 컴포넌트에 wrap 옵션 추가 및 문자 감싸기 분기 처리

--- src/components/common/TextSplitWrap.tsx
+++ src/components/common/TextSplitWrap.tsx
@@ -3,19 +3,28 @@ import React from 'react';
 export interface TextSplitWrapProps {
   text: string;
   className?: string;
+	wrap?: boolean;
   splitFunction: (text: string, className?: string) => React.ReactNode[];
 }
 
-export default function TextSplitWrap({ text, className, splitFunction }: TextSplitWrapProps) {
+export default function TextSplitWrap({ text, className, wrap = false, splitFunction }: TextSplitWrapProps) {
   const splitText = splitFunction(text, 'char');
 
+	if(wrap){
+		return (
+			<div className={className}>
+				{splitText.map((char, index) => (
+					<div key={index} style={{ overflow: 'hidden' }}>
+						{char}
+					</div>
+				))}
+			</div>
+		);
+	}
+
   return (
-    <div className={className}>
-      {splitText.map((char, index) => (
-        <div key={index} style={{ overflow: 'hidden' }}>
-          {char}
-        </div>
-      ))}
-    </div>
+		<div className={className}>
+			{splitText.map(char => char )}
+		</div>
   );
 } 
