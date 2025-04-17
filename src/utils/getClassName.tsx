f049cc9c63f068950729dd3395d1aa7b1e2ab8a7
2taeyoon <2tae@kakao.com>
2025-04-17 23:17:42 +0900

    refactor: getClassName 함수 props 타입 변경 및 클래스명 일부 수정

--- src/utils/getClassName.tsx
+++ src/utils/getClassName.tsx
@@ -1,12 +1,12 @@
-import { ProjectDataType } from "@/types/projectProps";
+import { CodeCardDataType } from "@/types/projectProps";
 
-export default function getClassName(baseClass: string, project?: ProjectDataType){
+export default function getClassName(baseClass: string, project?: CodeCardDataType){
 	if (!project) return baseClass;
 
 // ProjectLinks 컴포넌트에서 site와 github에 대한 width 클래스 처리
 if (baseClass === 'site' || baseClass === 'github') {
 	const widthClass = project.links?.site && project.links?.github ? 'width49' : 'width100';
-	if (project.linkBoxColor === 'var(--whiteDE)') return `${baseClass} ${widthClass} whiteDE`;
+	if (project.linkBoxColor === 'var(--whiteD)') return `${baseClass} ${widthClass} whiteD`;
 	if (project.linkBoxColor === 'var(--themaDeep)') return `${baseClass} ${widthClass} themaDeep`;
 	if (project.linkBoxColor === 'var(--thema)') return `${baseClass} ${widthClass} thema`;
 	if (project.linkBoxColor === 'var(--black333)') return `${baseClass} ${widthClass} black333`;
@@ -15,7 +15,7 @@ if (baseClass === 'site' || baseClass === 'github') {
 
 	// ProjectCard 컴포넌트에 프로젝트에 알맞는 클래스 추가
 	if (project.className === 'typortfolio') return `${baseClass} typortfolio`;
-	if (project.className === '2taeyoon') return `${baseClass} 2taeyoon`;
+	if (project.className === 'taeyoon') return `${baseClass} taeyoon`;
 	if (project.className === 'krmx') return `${baseClass} krmx`;
   if (project.className === 'hivu') return `${baseClass} hivu`;
 	if (project.className === 'niweb') return `${baseClass} niweb`;
