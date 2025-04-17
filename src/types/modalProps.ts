855ffd006f5e465b863f9f08bd2aa2bee7038b53
2taeyoon <2tae@kakao.com>
2025-04-17 23:22:19 +0900

    types: ImageModalProps 확장 및 모달 관련 타입 인터페이스 추가 정의

--- src/types/modalProps.ts
+++ src/types/modalProps.ts
@@ -1,8 +1,40 @@
 export interface ImageModalProps {
   isOpen: boolean;
   onClose: () => void;
-  screenshots: string[];
-  currentImageIndex: number;
+  imageUrl: string[];
+  title?: string;
+  date?: string;
+  type?: string;
+  contribution?: string;
+  currentImageIndex?: number;
+  onPrev?: () => void;
+  onNext?: () => void;
+	className?: string;
+} 
+
+export interface ModalButtonsProps {
+  currentIndex: number;
+  max: number;
+  isZoomed: boolean;
   onPrev: () => void;
   onNext: () => void;
-} 
+}
+
+export interface ModalImageProps {
+  src: string;
+  alt?: string;
+  onClick: () => void;
+}
+
+export interface ModalInfoProps {
+  title?: string;
+  date?: string;
+  type?: string;
+  contribution?: string;
+}
+
+export interface ModalOverlayProps {
+	children: React.ReactNode;
+  onClose: () => void;
+  className?: string;
+}
