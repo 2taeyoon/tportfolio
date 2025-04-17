a5f06dc35ccc22fc7ce6a675c7f602a8b1427d2b
2taeyoon <2tae@kakao.com>
2025-04-17 23:23:22 +0900

    feat: DesignCard 컴포넌트 생성 및 썸네일+내용 출력 구조 구현

---
+++ src/components/project/DesignCard.tsx
@@ -0,0 +1,30 @@
+import { DesignCardProps } from "@/types/projectProps"
+import Image from "next/image"
+
+export default function DesignCard({
+	project,
+	onClick
+}: DesignCardProps) {
+	return (
+		<div className="design_card" onClick={onClick}>
+			<div className="thumbnail">
+				<Image src={project.thumbnail} alt={project.title} draggable={false} fill
+					sizes="100%"
+					style={{ objectFit: 'cover', cursor: 'pointer' }}
+					onDragStart={(e) => e.preventDefault()}
+					onContextMenu={(e) => e.preventDefault()}
+				/>
+			</div>
+			<div className="content">
+				<div className="date_type_wrap">
+					<span className="type">{project.type}</span>
+					<span className="date">{project.date}</span>
+					<span className="contribution">{project.contribution}</span>
+					{project.research && <span className="research">{project.research}</span>}
+				</div>
+				<div className="title">{project.title}</div>
+				<p className="description">{project.description}</p>
+			</div>
+		</div>
+	)
+}
