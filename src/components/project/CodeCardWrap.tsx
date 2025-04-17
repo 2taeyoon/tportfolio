29f38dcc9acf6a8fd7c937cf56c132852321cbf0
2taeyoon <2tae@kakao.com>
2025-04-17 23:19:57 +0900

    refactor: CodeCardWrap 컴포넌트 명명 수정 및 index prop 제거

--- src/components/project/CodeCardWrap.tsx
+++ src/components/project/CodeCardWrap.tsx
@@ -6,19 +6,17 @@ import CodeCard from "@/components/project/CodeCard";
 import { characterTextSplit } from '@/utils/textSplit';
 import TextSplitWrap from '@/components/common/TextSplitWrap';
 
-export default function ProjectCardWrap() {
+export default function CodeCardWrap() {
 	const title = `PROJECT`;
 	const subTitle = `PORTFOLIO`;
 
 	return (
-		<div className="project_card_wrap">
-			<div className="code_card_list">
-				<TextSplitWrap text={title} className="code_card_title" splitFunction={characterTextSplit}/>
-				<TextSplitWrap text={subTitle} className="code_card_subtitle" splitFunction={characterTextSplit}/>
-				{projectCodeData.map((project, index) => (
-					<CodeCard key={index} index={index} project={project}/>
-				))}
-			</div>
+		<div className="code_card_wrap">
+			<TextSplitWrap text={title} className="code_card_title" splitFunction={characterTextSplit}/>
+			<TextSplitWrap text={subTitle} className="code_card_subtitle" splitFunction={characterTextSplit}/>
+			{projectCodeData.map((project, index) => (
+				<CodeCard key={index} project={project}/>
+			))}
 		</div>
 	)
 }
