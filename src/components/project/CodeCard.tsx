bc7fe61d9647adea0ff1755d211ae1d105c29d3d
2taeyoon <2tae@kakao.com>
2025-04-17 23:19:43 +0900

    refactor: CodeCard에 project.date 추가 및 index prop 제거

--- src/components/project/CodeCard.tsx
+++ src/components/project/CodeCard.tsx
@@ -7,12 +7,12 @@ import { CodeCardProps } from "@/types/projectProps";
 import DescriptionList from "@/components/project/DescriptionList";
 import getClassName from "@/utils/getClassName";
 
-export default function CodeCard({ project, index }: CodeCardProps) {
+export default function CodeCard({ project }: CodeCardProps) {
 	const { isModalOpen, currentImageIndex, handleImageClick, handlePrevImage, handleNextImage, handleCloseModal } = useImageModal();
 
 	return (
-		<div key={index} className={getClassName('code_card', project)} style={{ backgroundColor: project.boxColor }}>
-			<div className="card_title" style={{ color: project.fontColor }}>{project.title}</div>
+		<div className={getClassName('code_card', project)} style={{ backgroundColor: project.boxColor }}>
+			<div className="card_title" style={{ color: project.fontColor }}>{project.title}<span className="card_date">{project.date}</span></div>
 			<div className="card_type" style={{ backgroundColor: project.badgeBoxColor, color: project.badgeFontColor }}>{project.type}</div>
 			<TechStack project={project} rows={project.techStack.rows}/>
 			<SkillList project={project} title={project.skills.title} images={project.skills.images}/>
