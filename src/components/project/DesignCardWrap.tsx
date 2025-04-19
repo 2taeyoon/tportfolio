9ba6d4466afd29032fee9b4cc28e7cf606915d76
2taeyoon <2tae@kakao.com>
2025-04-19 13:06:32 +0900

    refactor: DesignCardWrap에서 ImageModal 제거 및 카드 클릭 기능 단순화

--- src/components/project/DesignCardWrap.tsx
+++ src/components/project/DesignCardWrap.tsx
@@ -2,37 +2,19 @@ import { characterTextSplit } from '@/utils/textSplit';
 import TextSplitWrap from '@/components/common/TextSplitWrap';
 import { projectDesignData } from "@/data/projectData";
 import DesignCard from "./DesignCard";
-import ImageModal from "@/components/modal/ImageModal";
-import { useImageModal } from "@/hooks/useImageModal";
 
 export default function DesignCardWrap() {
 	const title = `DESIGN`;
 	const subTitle = `PORTFOLIO`;
-
-	const { isModalOpen, currentImageIndex, handleImageClick, handlePrevImage, handleNextImage, handleCloseModal } = useImageModal();
 
 	return (
 		<div className="design_card_wrap">
 			<TextSplitWrap text={title} className="design_card_title" splitFunction={characterTextSplit}/>
 			<TextSplitWrap text={subTitle} className="design_card_subtitle" splitFunction={characterTextSplit}/>
 			{projectDesignData.map((project, index) => (
-				<DesignCard key={index} project={project} index={index} onClick={() => handleImageClick(index)}/>
+				<DesignCard key={index} project={project}/>
 			))}
-			{isModalOpen && (
-				<ImageModal
-					isOpen={isModalOpen}
-					onClose={handleCloseModal}
-					imageUrl={projectDesignData.map(item => item.thumbnail)}
-					title={projectDesignData[currentImageIndex].title}
-					date={projectDesignData[currentImageIndex].date}
-					type={projectDesignData[currentImageIndex].type}
-					contribution={projectDesignData[currentImageIndex].contribution}
-					currentImageIndex={currentImageIndex}
-					onPrev={handlePrevImage}
-					onNext={handleNextImage}
-					className="design"
-				/>
-			)}
+
 		</div>
 	);
 }
