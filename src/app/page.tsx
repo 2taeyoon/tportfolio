f041dd74f6f71675438f63e0c42f750f405270a0
2taeyoon <2tae@kakao.com>
2025-04-24 21:41:03 +0900

    feat: Home에 scroll refs 바인딩 및 Footer/NoiseBackground 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,31 @@
 'use client'
+
 import "@/style/style.css";
+
 import MainSection from "@/components/layout/MainSection";
 import ProfileSection from "@/components/layout/ProfileSection";
 import SkillSection from "@/components/layout/SkillSection";
 import ProjectSection from "@/components/layout/ProjectSection";
-// import { useEffect } from "react";
-// import gsap from "gsap";
+import Footer from "@/components/layout/Footer";
+import NoiseBackground from "@/components/common/Noise";
+import { useRef } from "react";
+
 export default function Home() {
-/*
-	useEffect(() => {
-    // 클라이언트에서 실행되므로 `document` 사용 가능
-    const gridContainer = document.querySelector(".grid-container");
+	const mainScrollRef = useRef<HTMLDivElement | null>(null);
+	const profileScrollRef = useRef<HTMLDivElement | null>(null);
+	const skillScrollRef = useRef<HTMLDivElement | null>(null);
+	const projectScrollRef = useRef<HTMLDivElement | null>(null);
 
-    if (!gridContainer) return;
-
-    const rows = 15;
-    const cols = 30;
-    const totalBoxes = rows * cols;
-    const boxes = [];
-
-    for (let i = 0; i < totalBoxes; i++) {
-      const box = document.createElement("div");
-      box.classList.add("grid-box");
-      gridContainer.appendChild(box);
-      boxes.push(box);
-    }
-
-    gsap.utils.shuffle(boxes);
-
-    gsap.to(boxes, {
-      opacity: 1,
-      duration: 0.001,
-      stagger: { each: 0.001, from: "random", },
-    });
-  }, []);
-*/
   return (
-    <div>
-			<MainSection/>
-			<ProfileSection/>
-			<SkillSection/>
-			<ProjectSection/>
-			<div style={{ height: "100vh", backgroundColor: "blueviolet" }}></div>
-			{/* <div className="grid-container"></div> */}
-    </div>
+    <>
+			<main>
+				<MainSection mainScrollRef={mainScrollRef}/>
+				<ProfileSection profileScrollRef={profileScrollRef}/>
+				<SkillSection skillScrollRef={skillScrollRef}/>
+				<ProjectSection projectScrollRef={projectScrollRef}/>
+			</main>
+			<Footer mainScrollRef={mainScrollRef} profileScrollRef={profileScrollRef} skillScrollRef={skillScrollRef} projectScrollRef={projectScrollRef}/>
+			<NoiseBackground/>
+		</>
   );
 }
