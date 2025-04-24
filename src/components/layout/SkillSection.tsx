a9ec7215f0efe69979f64e72e85cc3fd818c7d3a
2taeyoon <2tae@kakao.com>
2025-04-24 21:37:50 +0900

    refactor: SkillSection에 scrollRef prop 적용 및 Noise 컴포넌트 제거

--- src/components/layout/SkillSection.tsx
+++ src/components/layout/SkillSection.tsx
@@ -2,8 +2,7 @@
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useRef, useState } from 'react';
-import Noise from '@/components/common/Noise';
+import { useEffect, useState } from 'react';
 import SkillCircle from "@/components/skill/SkillCircle";
 import SkillDetail from "@/components/skill/SkillDetail";
 import DividerWithIcons from "@/components/common/DividerWithIcons";
@@ -13,11 +12,9 @@ import Wave from "../common/Wave";
 import { spaceTextSplit } from "@/utils/textSplit";
 import TextSplitWrap from "../common/TextSplitWrap";
 
-export default function SkillSection() {
+export default function SkillSection({skillScrollRef}: {skillScrollRef: React.RefObject<HTMLDivElement | null>}) {
 	const title = `MY SKILL`;
   const body = `시맨틱 태그를 활용한 구조적인 마크업 작성에 능숙하며 CSS(SCSS, Tailwind CSS)를 통해 반응형 웹, 애니메이션, 일관된 디자인 시스템을 구현할 수 있습니다. JavaScript와 TypeScript를 활용한 동적 UI 및 사용자 인터랙션 구현이 가능하며 React와 Next.js 기반의 컴포넌트 중심 개발과 SSR/SSG 최적화 경험을 갖추고 있습니다. Webpack을 통한 빌드 환경을 구성할 수 있으며 Git과 GitHub을 활용한 협업에도 익숙합니다. Firebase 및 Supabase를 통한 백엔드 연동도 경험해 보았습니다. Photoshop, Illustrator로 시각적 콘텐츠를 제작할 수 있으며 Markdown으로 Notion 문서 및 콘텐츠 작성도 가능합니다.`;
-
-	const skillScrollRef = useRef<HTMLDivElement | null>(null); // 스크롤 애니메이션이 적용될 요소
 
 	const [activeIndex, setActiveIndex] = useState(0); // 현재 가장 위에 있는 이미지 인덱스 (0번부터 시작)
 	const [radius, setRadius] = useState(400); // skill 원의 반지름 (이미지들이 얼마나 퍼져 있을지)
@@ -46,6 +43,8 @@ export default function SkillSection() {
 		gsap.registerPlugin(ScrollTrigger);
 
 		const ctx = gsap.context(() => {
+			if (!skillScrollRef?.current) return;
+
 			const q = gsap.utils.selector(skillScrollRef.current);
 			const titleCharRef = q('.my_skill_title .char'); // MySkillText 컴포넌트의 my_skill_title 클래스의 span과 my_skill_body 클래스의 span
 			const bodyCharRef = q('.my_skill_body .char'); // MySkillText 컴포넌트의 my_skill_title 클래스의 span과 my_skill_body 클래스의 span
@@ -99,7 +98,7 @@ export default function SkillSection() {
 		});
 
 		return () => ctx.revert();
-	}, []);
+	}, [skillScrollRef]);
 
 	const { translate2, handleMouseMove } = useMouseEffect(); // 마우스 이펙트 훅
 
@@ -117,7 +116,6 @@ export default function SkillSection() {
 				<SkillDetail radius={radius} itemSize={itemSize} activeIndex={activeIndex}/>
       </div>
 			<Wave />
-      <Noise/>
     </div>
   );
 }
