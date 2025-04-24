6a5590f3834c3f27051f18c56bbe40882869db07
2taeyoon <2tae@kakao.com>
2025-04-24 21:36:17 +0900

    feat: ScrollRefProps 인터페이스 추가 (Add ScrollRefProps interface)

--- src/types/props.ts
+++ src/types/props.ts
@@ -13,3 +13,10 @@ export interface GsabRefProps {
 	secondTextRefs?: RefObject<(HTMLDivElement | null)[]>;
 	textRef?: RefObject<(HTMLDivElement | null)[]>;
 }
+
+export interface ScrollRefProps {
+	mainScrollRef?: React.RefObject<HTMLDivElement | null>;
+	profileScrollRef?: React.RefObject<HTMLDivElement | null>;
+	skillScrollRef?: React.RefObject<HTMLDivElement | null>;
+  projectScrollRef?: React.RefObject<HTMLDivElement | null>;
+}
