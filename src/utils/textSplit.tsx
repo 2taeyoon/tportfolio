5eaa6923f3ef3e678914a4c6039d4503ded35b4b
2taeyoon <2tae@kakao.com>
2025-04-24 22:23:16 +0900

    refactor: spaceTextSplit 함수에서 flatMap 방식으로 공백 렌더링 방식 개선 (공백 누락 문제 해결)

--- src/utils/textSplit.tsx
+++ src/utils/textSplit.tsx
@@ -6,10 +6,12 @@ export function characterTextSplit(text: string, className: string = "") {
   ));
 }
 
-export function spaceTextSplit(text: string, className: string = "") {
-  return text.split(" ").map((word, index) => (
-    <span key={index} className={className} style={{ display: 'inline-block' }}>
-			{`${word} `}
-		</span>
-  ));
+export function spaceTextSplit(text: string, className?: string) {
+  const words = text.split(" ");
+  return words.flatMap((word, index) => [
+    <span key={`word_${index}`} className={className}>
+      {word}
+    </span>,
+    index !== words.length - 1 && " ", // 마지막 단어 뒤엔 공백 안 넣음
+  ]);
 }
