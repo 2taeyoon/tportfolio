3c4810df1705e088f65d54e3a535f43cdc8c4e90
2taeyoon <2tae@kakao.com>
2025-04-25 22:14:48 +0900

    feat: Navigation 컴포넌트 생성 및 스크롤 이동 기능 적용

---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,27 @@
+import { useScrollToSection } from "@/hooks/useScrollToSection";
+import { ScrollRefProps } from "@/types/commonProps";
+
+export default function Navigation({ profileScrollRef, skillScrollRef, projectScrollRef }: ScrollRefProps) {
+  const { scrollToSection } = useScrollToSection();
+
+	// MainSection에서는 pin을 이용하여 스크롤 트리거를 적용중이라 scrollIntoView를 적용하면
+	// 해당 요소의 끝에서 멈추므로 부득이하게 scrollTo로 대체
+  const navItems = [
+    { label: 'Main', action: () => window.scrollTo({ top: 0, behavior: 'smooth' }) },
+    { label: 'Profile', action: () => profileScrollRef && scrollToSection(profileScrollRef) },
+    { label: 'Skill', action: () => skillScrollRef && scrollToSection(skillScrollRef) },
+    { label: 'Project', action: () => projectScrollRef && scrollToSection(projectScrollRef) }
+  ];
+
+  return (
+    <nav className="nav">
+      <ul>
+        {navItems.map((item) => (
+          <li key={item.label} onClick={item.action}>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
