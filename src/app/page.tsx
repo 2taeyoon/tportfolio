98159c4f9d7d19fe1eb6552feffcbf452f0e3d37
2taeyoon <2tae@kakao.com>
2025-04-26 01:36:52 +0900

    refactor: Footer props 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 
 import "@/style/style.css";
 
+import NoiseBackground from "@/components/common/Noise";
 import MainSection from "@/components/layout/MainSection";
 import ProfileSection from "@/components/layout/ProfileSection";
 import SkillSection from "@/components/layout/SkillSection";
 import ProjectSection from "@/components/layout/ProjectSection";
 import Footer from "@/components/layout/Footer";
-import NoiseBackground from "@/components/common/Noise";
+//import Header from "@/components/layout/Header";
+
 import { useRef } from "react";
+//import { useModalStore } from "@/hooks/useModalStore";
 
 export default function Home() {
 	const mainScrollRef = useRef<HTMLDivElement | null>(null);
@@ -17,15 +20,18 @@ export default function Home() {
 	const projectScrollRef = useRef<HTMLDivElement | null>(null);
 	const footerScrollRef = useRef<HTMLDivElement | null>(null);
 
+	//const { isAnyModalOpen } = useModalStore();
+
   return (
     <>
+			{/* {!isAnyModalOpen && <Header profileScrollRef={profileScrollRef} skillScrollRef={skillScrollRef} projectScrollRef={projectScrollRef}/>} */}
 			<main>
 				<MainSection mainScrollRef={mainScrollRef}/>
 				<ProfileSection profileScrollRef={profileScrollRef}/>
 				<SkillSection skillScrollRef={skillScrollRef}/>
 				<ProjectSection projectScrollRef={projectScrollRef}/>
 			</main>
-			<Footer mainScrollRef={mainScrollRef} profileScrollRef={profileScrollRef} skillScrollRef={skillScrollRef} projectScrollRef={projectScrollRef} footerScrollRef={footerScrollRef}/>
+			<Footer profileScrollRef={profileScrollRef} skillScrollRef={skillScrollRef} projectScrollRef={projectScrollRef} footerScrollRef={footerScrollRef}/>
 			<NoiseBackground/>
 		</>
   );
