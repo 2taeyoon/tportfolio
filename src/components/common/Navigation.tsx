042d610fb6b4d066ced8415cf109d3b5c93f1af7
2taeyoon <2tae@kakao.com>
2025-04-29 01:21:48 +0900

    style: Navigation 메뉴 텍스트를 소문자에서 대문자로 변경

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -7,10 +7,10 @@ export default function Navigation({ profileScrollRef, skillScrollRef, projectSc
 	// MainSection에서는 pin을 이용하여 스크롤 트리거를 적용중이라 scrollIntoView를 적용하면
 	// 해당 요소의 끝에서 멈추므로 부득이하게 scrollTo로 대체
   const navItems = [
-    { label: 'Main', action: () => window.scrollTo({ top: 0, behavior: 'smooth' }) },
-    { label: 'Profile', action: () => profileScrollRef && scrollToSection(profileScrollRef) },
-    { label: 'Skill', action: () => skillScrollRef && scrollToSection(skillScrollRef) },
-    { label: 'Project', action: () => projectScrollRef && scrollToSection(projectScrollRef) }
+    { label: 'MAIN', action: () => window.scrollTo({ top: 0, behavior: 'smooth' }) },
+    { label: 'PROFILE', action: () => profileScrollRef && scrollToSection(profileScrollRef) },
+    { label: 'SKILL', action: () => skillScrollRef && scrollToSection(skillScrollRef) },
+    { label: 'PROJECT', action: () => projectScrollRef && scrollToSection(projectScrollRef) }
   ];
 
   return (
