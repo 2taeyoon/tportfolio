9777578eac96b68eedacbe8e921a20a88dca1d36
2taeyoon <2tae@kakao.com>
2025-05-23 00:28:19 +0900

    chore: 학력 및 경력사항 임시 주석 처리하여 교육사항만 노출되도록 수정

--- src/components/profile/list/ExperienceList.tsx
+++ src/components/profile/list/ExperienceList.tsx
@@ -3,14 +3,14 @@ import ExperienceItem from "@/components/profile/list/ExperienceItem";
 import useMouseEffect from "@/hooks/useMouseEffect";
 import ShapeQuoteDots from "@/components/profile/ShapeQuoteDots";
 import Copyright from "@/components/common/Copyright";
-import { careerData, educationData, schoolData } from "@/data/profileData";
+import { /*careerData,*/ educationData, /*schoolData*/ } from "@/data/profileData";
 
 export default function ExperienceList() {
 	const { translate, handleMouseMove } = useMouseEffect(); // 마우스 이펙트 훅
   return (
     <div className="experience_wrap pd_box" onMouseMove={handleMouseMove}>
-      <ExperienceItem title="학력사항" items={schoolData} translate={translate}/>
-      <ExperienceItem title="경력사항" extra="(8개월)" items={careerData} translate={translate}/>
+      {/* <ExperienceItem title="학력사항" items={schoolData} translate={translate}/> */}
+      {/* <ExperienceItem title="경력사항" extra="(8개월)" items={careerData} translate={translate}/> */}
       <ExperienceItem title="교육사항" items={educationData} translate={translate}/>
 			<Copyright translate={translate}/>
 			<div className="shape_quote_dots_wrap">
