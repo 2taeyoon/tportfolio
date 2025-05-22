f211ce6360b61ff63cd0a4adc39cb0f93c7e663e
2taeyoon <2tae@kakao.com>
2025-05-23 00:27:39 +0900

    feat: Navigation 컴포넌트에 외부 링크 메뉴(GITHUB, BLOG) 추가 및 스타일 클래스 분리

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,5 +1,6 @@
 import { useScrollToSection } from "@/hooks/useScrollToSection";
 import { ScrollRefProps } from "@/types/commonProps";
+import Link from "next/link";
 
 export default function Navigation({ profileScrollRef, skillScrollRef, projectScrollRef }: ScrollRefProps) {
   const { scrollToSection } = useScrollToSection();
@@ -7,20 +8,34 @@ export default function Navigation({ profileScrollRef, skillScrollRef, projectSc
 	// MainSection에서는 pin을 이용하여 스크롤 트리거를 적용중이라 scrollIntoView를 적용하면
 	// 해당 요소의 끝에서 멈추므로 부득이하게 scrollTo로 대체
   const navItems = [
-    { label: 'MAIN', action: () => window.scrollTo({ top: 0, behavior: 'smooth' }) },
-    { label: 'PROFILE', action: () => profileScrollRef && scrollToSection(profileScrollRef) },
-    { label: 'SKILL', action: () => skillScrollRef && scrollToSection(skillScrollRef) },
-    { label: 'PROJECT', action: () => projectScrollRef && scrollToSection(projectScrollRef) }
+    { label: 'MAIN', class: 'scroll', action: () => window.scrollTo({ top: 0, behavior: 'smooth' }) },
+    { label: 'PROFILE', class: 'scroll', action: () => profileScrollRef && scrollToSection(profileScrollRef) },
+    { label: 'SKILL', class: 'scroll', action: () => skillScrollRef && scrollToSection(skillScrollRef) },
+    { label: 'PROJECT', class: 'scroll', action: () => projectScrollRef && scrollToSection(projectScrollRef) }
   ];
+
+	const anotherNavItems = [
+		{ label: 'GITHUB', class: 'another', href: 'https://github.com/2taeyoon' },
+		{ label: 'BLOG', class: 'another', href: 'https://www.2taeyoon.com/' }
+	];
 
   return (
     <nav className="nav">
       <ul>
         {navItems.map((item) => (
           <li key={item.label} onClick={item.action}>
-            {item.label}
+            <div className={item.class}>
+							{item.label}
+						</div>
           </li>
         ))}
+				{anotherNavItems.map((item) => (
+					<li key={item.label}>
+						<Link href={item.href} className={item.class} target="_blank" rel="noopener noreferrer">
+							{item.label}
+						</Link>
+					</li>
+				))}
       </ul>
     </nav>
   );
