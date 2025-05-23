2719dc648645ed8456e3bc2388c4e9292a13f09e
2taeyoon <2tae@kakao.com>
2025-05-24 02:17:20 +0900

    perf: 스크롤 트리거 애니메이션 범위 조정으로 텍스트 등장 시점 개선

--- src/components/layout/SkillSection.tsx
+++ src/components/layout/SkillSection.tsx
@@ -66,7 +66,7 @@ export default function SkillSection({skillScrollRef}: {skillScrollRef: React.Re
 				scrollTrigger: {
 					trigger: titleCharRef,
 					start: "top bottom", // 트리거 요소의 A가 뷰포트(windiw)의 B에 닿을 때 시작
-					end: "top top+=400", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
+					end: "top top+=300", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
 					scrub: 3, // 값이 클수록 더 부드럽게 움직임(기본값 true)(스크롤 반대 시에도 애니메이션 작동)
 					//markers: true, // 스크롤 확인용(개발 모드에서만 사용)
 				},
@@ -78,7 +78,7 @@ export default function SkillSection({skillScrollRef}: {skillScrollRef: React.Re
 				scrollTrigger: {
 					trigger: bodyCharRef,
 					start: "top bottom", // 트리거 요소의 A가 뷰포트(windiw)의 B에 닿을 때 시작
-					end: "top top+=400", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
+					end: "top top+=300", // 트리거 요소의 A가 뷰포트(window)의 B에 닿을 때 종료					
 					scrub: 3, // 값이 클수록 더 부드럽게 움직임(기본값 true)(스크롤 반대 시에도 애니메이션 작동)
 					//markers: true, // 스크롤 확인용(개발 모드에서만 사용)
 				},
